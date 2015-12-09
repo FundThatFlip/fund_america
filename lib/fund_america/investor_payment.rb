bafc30fc7f5107f19f117b8fd1ccdad646a7c035
Dan M <dan987@aol.com>
2015-12-09 17:22:40 -0500

    description

--- lib/fund_america/investor_payment.rb
+++ lib/fund_america/investor_payment.rb
@@ -10,10 +10,10 @@ module FundAmerica
       end
 
       # End point: https://apps.fundamerica.com/api/investor_payments/:id (GET)
-      # Usage: FundAmerica::InvestorPayment.details(investor_payment_id)
-      # Output: Returns the details of an investor_payment with matching id
+      # Usage: FundAmerica::InvestorPayment.details(investor_payment_id, "&_expand[<attribute_name>]=1")
+      # Output: Returns the details of an investor_payment with matching id, and special request options
       def details(investor_payment_id, request_opitions = "")
-        API::request(:get, FundAmerica.base_uri + "investor_payments/#{investor_payment_id}" + "#{request_options}"))
+        API::request(:get, FundAmerica.base_uri + "investor_payments/#{investor_payment_id}" + "#{request_options}")
       end
 
     end
